de76c232067373921371e2dd4a30e3da6396fe3f
Matteo Michelon <michelon.matteo89@gmail.com>
2021-04-29 14:05:52 +0200

    Mileston 1.2 completata, ma corretta??

--- js/script.js
+++ js/script.js
@@ -7,13 +7,44 @@ var app = new Vue(
         /* DATA */
         data: {
 
-            pushText: ''
+            pushText: '',
 
         },
         /* end DATA */
 
         /* METHODS */
         methods: {
+
+            /* Funzione callApi */
+            // Funzione per richiamare API
+            callApi () {
+
+                // Dichiaro la variabile query con il valore
+                // inserito dall'utente
+                const query = this.pushText;
+
+                axios
+                .get( 'https://api.themoviedb.org/3/search/movie', {
+
+                    params: {
+                        api_key: '3414ee67882ebd632253f10b916225d8',
+                        language: 'it',
+                        query: query,
+                        page: 1
+                    }
+
+                } )
+                .then( ( response ) => {
+
+                    // Secondo l'url inserito avremo l'array di oggetti che ci interessa in:
+                    // response.data.results
+                    const arrayFilm = response.data.results
+                    console.log( response.data.results );
+
+                } );
+
+            }
+            /* end Funzione callApi */
 
         },
         /* end METHODS */
@@ -23,7 +54,16 @@ var app = new Vue(
 
             /* Axios */
             axios
-                .get( 'https://api.themoviedb.org/3/search/movie?&api_key=3414ee67882ebd632253f10b916225d8&language=it&query=ritorno al futuro&page=1' )
+                .get( 'https://api.themoviedb.org/3/search/movie', {
+
+                    params: {
+                        api_key: '3414ee67882ebd632253f10b916225d8',
+                        language: 'it',
+                        query: 'test',
+                        page: 1
+                    }
+
+                } )
                 .then( ( response ) => {
 
                     // Secondo l'url inserito avremo l'array di oggetti che ci interessa in:
@@ -67,3 +107,7 @@ film trovato:
 /* Milestone 1.1 */
 // Creare un layout base con una searchbar (una input e un button) in cui possiamo 
 // scrivere completamente o parzialmente il nome di un film.
+
+/* Mileston 1.2 */
+// Possiamo, cliccando il bottone, 
+// cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
