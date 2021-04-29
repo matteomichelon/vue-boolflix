b256d143b7e0c3879ef292c7ef9fb08f9d43a93d
Matteo Michelon <michelon.matteo89@gmail.com>
2021-04-29 12:55:07 +0200

    Mileston 1.1 completata

--- js/script.js
+++ js/script.js
@@ -7,26 +7,41 @@ var app = new Vue(
         /* DATA */
         data: {
 
+            pushText: ''
+
         },
+        /* end DATA */
 
         /* METHODS */
         methods: {
 
         },
+        /* end METHODS */
 
         /* MOUNTED */
         mounted () {
+
+            /* Axios */
             axios
-                .get( 'url' )
+                .get( 'https://api.themoviedb.org/3/search/movie?&api_key=3414ee67882ebd632253f10b916225d8&language=it&query=ritorno al futuro&page=1' )
                 .then( ( response ) => {
-                    const result = response.data;
+
+                    // Secondo l'url inserito avremo l'array di oggetti che ci interessa in:
+                    // response.data.results
+                    const arrayFilm = response.data.results
+                    console.log( response.data.results );
+
                 } );
+            /* end Axios */
+
         },
+        /* end MOUNTED */
 
         /* CREATED */
-        created(){
+        created () {
 
         }
+        /* end CREATED */
 
     }
 );
@@ -34,3 +49,21 @@ var app = new Vue(
 /* end Vue */
 
 
+//====
+/*
+Milestone 1:
+Creare un layout base con una searchbar (una input e un button) in cui possiamo
+scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
+bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
+film trovato:
+1. Titolo
+2. Titolo Originale
+3. Lingua
+4. Voto
+*/
+//====
+
+/* Milestone 1.1 */
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo 
+// scrivere completamente o parzialmente il nome di un film.
