fdc9ede93bdc1e79923caef764b974ac4b58eb6a
Matteo Michelon <michelon.matteo89@gmail.com>
2021-04-30 09:44:43 +0200

    Mileston 3.1 completata

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ var app = new Vue(
 
             // Ricerca Film
             // /search/movie
+            // Passare come parametro il valore di query
             film ( text ) {
                 axios
                     .get( 'https://api.themoviedb.org/3/search/movie', {
@@ -51,6 +52,7 @@ var app = new Vue(
 
             // Ricerca Telefilm
             // /search/tv
+            // Passare come parametro il valore di query
             telefilm ( text ) {
                 axios
                     .get( 'https://api.themoviedb.org/3/search/tv', {
@@ -172,5 +174,38 @@ crubs
 // (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 // Qui un esempio di chiamata per le serie tv:
 // https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
+//===
+/* 
+Mileston 3;
+In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
+al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
+perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+Dovremo prendere quindi l’URL base delle immagini di TMDB:
+https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
+(troviamo tutte le dimensioni possibili a questo link:
+https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la
+parte finale dell’URL passata dall’API.
+Esempio di URL:
+https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+Trasformiamo poi il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
+piene (o mezze vuote :P)
+*/
+//===
+
+/* Mileston 3.1 */
+// In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
+// al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
+// perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+// Dovremo prendere quindi l’URL base delle immagini di TMDB:
+// https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
+// (troviamo tutte le dimensioni possibili a questo link:
+// https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la
+// parte finale dell’URL passata dall’API.
+// Esempio di URL:
+// https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
 
 
